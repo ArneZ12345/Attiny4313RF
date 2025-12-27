7760532086611a0ad751585386ccfcf50bf55817
ArneZ12345 <zrimsek.arne@gmail.com>
2025-12-27 13:29:33 +0100

    Added user defined delay
    Added user defined delay between each message stream

--- Transmitter/Transmit.cpp
+++ Transmitter/Transmit.cpp
@@ -1,7 +1,8 @@
 #include "Main.h"	
 #include "TinyRF_TX.h"
+#include "Settings.h"
 
 void DHT_TRANSMIT_VALUES(){
-  send((byte*)message, strlen(message)+1);
-  delayMicroseconds(TX_DELAY_MICROS);
+    send((byte*)message, strlen(message)+1);
+  delay(USER_DEF_DELAY);
 }
